888730d90f9e7b544a035246779f0eefc03646ca
JBH <qjagh95@naver.com>
2019-06-18 19:39:48 +0900

    리스트 수정

--- MyList/MyList.cpp
+++ MyList/MyList.cpp
@@ -8,4 +8,14 @@ int main()
 {
 	MyList<int> a;
 
+	for (size_t i = 0; i < 10; i++)
+		a.Push_Back(i);
+
+	MyList<int>::Iterator StartIter = a.Begin();
+	MyList<int>::Iterator EndIter = a.End();
+
+	for (; StartIter != EndIter; StartIter++)
+	{
+		cout << (*StartIter) << endl;
+	}
 }
--- MyList/MyList.h
+++ MyList/MyList.h
@@ -7,11 +7,10 @@ private:
 	class Node
 	{
 	public:
-		Node() :m_Next(NULL), m_Prev(NULL) {}
-		Node(const T& _Data) :m_Data(_Data), m_Next(NULL), m_Prev(NULL) {}
-		~Node();
+		Node() :m_Next(nullptr), m_Prev(nullptr) {}
+		Node(const T& Data) :m_Data(Data), m_Next(nullptr), m_Prev(nullptr) {}
+		~Node() {}
 
-	private:
 		T m_Data;
 		Node* m_Next;
 		Node* m_Prev;
@@ -19,13 +18,30 @@ private:
 		friend MyList;
 	};
 
+public:
 	class Iterator
 	{
 	public:
 		Iterator(const Iterator& Iter) { m_CurNode = Iter; }
+		Iterator(Node* node) { m_CurNode = node; }
 
-		Iterator& operator++() { m_CurNode = m_CurNode->m_Next; return *this; }
-		Iterator& operator--() { m_CurNode = m_CurNode->m_Prev; return *this; }
+		Iterator& operator++(int) 
+		{ 
+			m_CurNode = m_CurNode->m_Next;
+			return *this;
+		}
+		Iterator& operator++()
+		{
+			m_CurNode = m_CurNode->m_Next;
+			return *this;
+		}
+		Iterator& operator--() 
+		{ 
+			m_CurNode = m_CurNode->m_Prev;
+			return *this;
+		}
+		bool operator!=(const Iterator& Iter) { return m_CurNode != Iter.m_CurNode; }
+		T& operator*() { return m_CurNode->m_Data; }
 
 	private:
 		Node* m_CurNode;
@@ -38,16 +54,16 @@ public:
 	{
 		m_Size = 0;
 
-		m_First->m_Next = m_Last;
-		m_First->m_Prev = nullptr;
+		m_First.m_Next = &m_Last;
+		m_First.m_Prev = nullptr;
 
-		m_Last->m_Prev = m_First;
-		m_Last->m_First = nullptr;
+		m_Last.m_Prev = &m_First;
+		m_Last.m_Next = nullptr;
 	}
 
 	void Push_Back(const T& Data)
 	{
-		T* newData = new T(Data);
+		Node* newData = new Node(Data);
 
 		newData->m_Prev = m_Last.m_Prev;
 		newData->m_Next = &m_Last;
@@ -58,7 +74,7 @@ public:
 
 	void Front_Back(const T& Data)
 	{
-		T* newData = new T(Data);
+		Node* newData = new Node(Data);
 
 		newData->m_Next = m_First.m_Next;
 		newData->m_Prev = &m_First;
@@ -69,7 +85,7 @@ public:
 
 	void Insert(Iterator& Iter, const T& Data)
 	{
-		T* newData = new T(Data);
+		Node* newData = new Node(Data);
 		Node* getNode = Iter.m_CurNode;
 
 		newData->m_Prev = getNode;
@@ -87,8 +103,11 @@ public:
 		DeleteNode->m_Prev->m_Next = DeleteNode->m_Next;
 	}
 
+	Iterator Begin() { return m_First.m_Next; }
+	Iterator End() { return &m_Last; }
+
 private:
-	T m_First;
-	T m_Last;
+	Node m_First;
+	Node m_Last;
 	size_t m_Size;
 };
