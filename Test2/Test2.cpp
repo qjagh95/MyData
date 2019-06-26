5dbdc605b9c85411ef2d42013189a5049fe82333
JBH <qjagh95@naver.com>
2019-06-26 19:08:51 +0900

    C++테스트

---
+++ Test2/Test2.cpp
@@ -0,0 +1,45 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+class A
+{
+public:
+	void Print() { cout << "A 프린트" << endl; }
+};
+
+class AA
+{
+public:
+	virtual void Print() { cout << "AA" << endl; }
+	AA() { Print(); }
+
+};
+class BB : public AA
+{
+public:
+	void Print() override { cout << "BB" << endl; }
+	BB() { Print(); }
+
+};
+class CC : public BB
+{
+public:
+	void Print() override { cout << "CC" << endl; }
+	CC() { Print(); }
+};
+
+int main()
+{
+	//A* newA = nullptr;
+	//실행됨(this콜 함수내에서 참조하는 변수가 없기때문에 실행가능)
+	//newA->Print();
+
+	AA* newAA = new CC();
+	//함수 뒤 this가 붙기때문에 AA BB CC CC실행
+	newAA->Print();
+
+	delete newAA;
+}
+
+
