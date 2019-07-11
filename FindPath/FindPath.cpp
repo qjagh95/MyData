3795fafed1507df3cb80235adeb39835822d78b4
JBH <qjagh95@naver.com>
2019-07-11 18:45:54 +0900

    TEST

---
+++ FindPath/FindPath.cpp
@@ -0,0 +1,70 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <list>
+
+using namespace std;
+
+enum TILE_OPTION
+{
+	TO_NONE,
+	TO_NOMOVE,
+	TO_ITEM,
+};
+
+struct Edge;
+struct Tile2D
+{
+	int x;
+	int y;
+	int Weight;
+	Tile2D* Prev;
+	Tile2D* Next;
+
+	TILE_OPTION Option;
+	vector<Edge*> VecEdge;
+
+	Tile2D()
+	{
+		x = 0;
+		y = 0;
+		Weight = 0;
+		Option = TO_NONE;
+		VecEdge.reserve(4);
+
+		Prev = nullptr;
+	}
+};
+
+struct Edge
+{
+	Tile2D* From;
+	Tile2D* Target;
+	Edge* Next;
+
+	int Weight;
+
+	Edge()
+	{
+		From = nullptr;
+		Target = nullptr;
+		Weight = 0;
+	}
+};
+
+vector<Tile2D*> VecItem;
+
+int Func(vector<vector<int>> Mase)
+{
+	int Distance = 0;
+
+	return Distance;
+}
+
+int main()
+{
+}
+
--- TextFile/TextFile.cpp
+++ TextFile/TextFile.cpp
@@ -1,10 +1,45 @@
 ﻿#include "pch.h"
 #include <iostream>
-#include <sstream>
-
+#include <fstream>
+#include <string>
 using namespace std;
+
+void Swap(int& a, int& b)
+{
+	a ^= b;
+	b ^= a;
+	a ^= b;
+}
 
 int main()
 {
-	ifstream asbc;
+	ifstream InputFile;
+	InputFile.open("Test.txt", ios::in);
+
+	int a = 0;
+	float b = 0;
+	string c;
+
+	char Buffer[255] = {};
+	InputFile.getline(Buffer, 10);
+
+	string Temp = Buffer;
+	Temp.erase(0, Temp.find_last_of(" "));
+	a = stoi(Temp);
+
+	InputFile.getline(Buffer, 10);
+
+	Temp = Buffer;
+	Temp.erase(0, Temp.find_last_of(" "));
+	b = stoi(Temp);
+
+	InputFile.getline(Buffer, 20);
+
+	Temp = Buffer;
+	Temp.erase(0, Temp.find_last_of(" "));
+	c = Temp;
+
+	int ccc = 100;
+	int ddd = 200;
+	Swap(ccc, ddd);
 }
